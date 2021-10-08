e7818d8a6915e072d56ed5a0a554cc8d35b2616b
thayron <thayronf3@gmail.com>
2021-10-08 04:20:40 -0300

    Foi criado o button de Sigin do git "Ainda nao funcional" ;

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,15 @@
 import styles from "./styles.module.scss";
-
+import { SignInButton } from '../SignInButton'
 export function Header() {
   return (
     <header className={styles.hreaderContainer}>
-      <div className={styles.hreaderContent }>
-        <img src="/images/logo.svg" alt="" />
+      <div className={styles.hreaderContent}>
+        <img src="/images/logo.svg" />
         <nav>
           <a className={styles.active}>Home</a>
           <a>Posts</a>
         </nav>
+        <SignInButton />
       </div>
     </header >
   );
