98fd40b2a7b24f59c7f7a9199e2f018d9a7f4d57
thayron <thayronf3@gmail.com>
2021-10-14 07:43:18 -0300

    Foi utilizado *Static site Generation* (SSG)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,8 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
-import { SignInButton } from '../components/SignInButton';
 import { SubscribeButton } from '../components/SubscribeButton';
-import { GetServerSideProps } from 'next';
-import styles from './home.module.scss';
 import { stripe } from '../services/stripe';
+import styles from './home.module.scss';
 
 interface HomeProps {
   product: {
@@ -34,7 +33,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1JkQbGDBlicIrjToRGguLSKw');
 
   const product = {
@@ -48,6 +47,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24 // 24 horas
   }
 }
